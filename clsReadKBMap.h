250611415f479cc2246ca47c736999305a1b96ea
dan-shaffer <shaffer2112@gmail.com>
2015-01-24 10:51:38 -0500

    Add read wse class

---
+++ clsReadKBMap.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   clsReadKBMap.h
+ * Author: Dan
+ *
+ * Created on January 24, 2015, 10:43 AM
+ */
+
+#ifndef CLSREADKBMAP_H
+#define	CLSREADKBMAP_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+class clsReadKBMap {
+public:
+    clsReadKBMap();
+    clsReadKBMap(const clsReadKBMap& orig);
+    virtual ~clsReadKBMap();
+    std::string KBDescription;
+    std::string WSEFileName;
+private:
+    int NumberOfKeys;
+
+};
+
+#endif	/* CLSREADKBMAP_H */
+
